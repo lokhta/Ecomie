380a68f72948b64138a7912d3cd8e9fec286d039
Sofiane Al amri <sa.dev67@gmail.com>
2020-02-29 12:19:32 +0100

    Correction url img home

--- application/views/templates_c/b091d690faab2d9daf76ad6d9e684f05a85140b4_0.file.home.tpl.php
+++ application/views/templates_c/b091d690faab2d9daf76ad6d9e684f05a85140b4_0.file.home.tpl.php
@@ -1,5 +1,27 @@
-        {include file="header.tpl" title="Ecomie - Accueil" name=$Name}
-        <div id=background>
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-02-29 11:15:46
+  from 'C:\wamp64\www\Ecomie\application\views\templates\pages\home.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5e5a47e2d9ac70_51843363',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'b091d690faab2d9daf76ad6d9e684f05a85140b4' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Ecomie\\application\\views\\templates\\pages\\home.tpl',
+      1 => 1582974858,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e5a47e2d9ac70_51843363 (Smarty_Internal_Template $_smarty_tpl) {
+?>        <div id=background>
             <div class="slider">
                 <div class="slides">
 
@@ -20,15 +42,15 @@
         </div>
         <section id="impact">
             <div class="terre">
-                <img class="centrer" src="assets/img/terre.png" alt="Terre">
+                <img class="centrer" src="img/terre.png" alt="Terre">
                 <p>réduison notre impact</br>sur la planéte</br>par la force du groupe</p>
             </div>
             <div class="terre">
-                <img src="assets/img/partage_2.png" alt="Partage">
+                <img src="img/partage_2.png" alt="Partage">
                 <p>Apprendre et partager </br> avec ses proches</p>
             </div>
             <div class="terre">
-                <img src="assets/img/qartier.png" alt="Quartier">
+                <img src="img/qartier.png" alt="Quartier">
                 <p>Ce retrouver lors </br>d'événements et défi</br>de quartier convivial</p>
             </div>
         </section>
@@ -40,4 +62,5 @@
                 <button class="bouton">Souscrivez </button>
             </div>
         </section>
-        {include file="footer.tpl"}
+<?php }
+}
