89b0fde2d82ba6d33a62ff0f824c593df9ba5b7d
Sofiane Al amri <sa.dev67@gmail.com>
2020-02-29 11:38:29 +0100

    cache

--- application/views/templates_c/89ee96fb91650e4f656059632a63f549b61a4a7b_0.file.footer.tpl.php
+++ application/views/templates_c/89ee96fb91650e4f656059632a63f549b61a4a7b_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-02-29 10:32:12
+/* Smarty version 3.1.34-dev-7, created on 2020-02-29 10:34:47
   from 'C:\wamp64\www\Ecomie\application\views\templates\footer.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5e5a3dac232570_05894275',
+  'unifunc' => 'content_5e5a3e4739aec5_25105748',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '89ee96fb91650e4f656059632a63f549b61a4a7b' => 
     array (
       0 => 'C:\\wamp64\\www\\Ecomie\\application\\views\\templates\\footer.tpl',
-      1 => 1582972295,
+      1 => 1582972359,
       2 => 'file',
     ),
   ),
@@ -20,12 +20,12 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5e5a3dac232570_05894275 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5e5a3e4739aec5_25105748 (Smarty_Internal_Template $_smarty_tpl) {
 ?>        <footer>
             <div id="footer_contenaire">
                 <div id="footer__fb">
                     <div class="fb">
-                        <a href="#"><img src="img/fb.svg" alt="Facebook"></a>
+                        <a href="#"><img src="assets/img/fb.svg" alt="Facebook"></a>
                     </div>
                     <ul>
                         <li><a href="CGU/cgu.html">Mention légal</a></li>
