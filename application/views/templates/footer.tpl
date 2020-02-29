11bf0d9a37ff3512b7aabadae83a3aafda9ce0f4
Sofiane Al amri <sa.dev67@gmail.com>
2020-02-29 11:32:05 +0100

    Déplacement du footer dans footer.tpl

--- application/views/templates/footer.tpl
+++ application/views/templates/footer.tpl
@@ -1,2 +1,39 @@
+        <footer>
+            <div id="footer_contenaire">
+                <div id="footer__fb">
+                    <div class="fb">
+                        <a href="#"><img src="img/fb.svg" alt="Facebook"></a>
+                    </div>
+                    <ul>
+                        <li><a href="CGU/cgu.html">Mention légal</a></li>
+                        <li><a href="CGU/cgu.html">CGU</a></li>
+                        <li><a href="plan/plan.html">Plan du site</a></li>
+                    </ul>
+                </div>
+                <div id="footer__adress">
+                    <ul>
+                        <li>Ecomie</li>
+                        <li>27 rue de la république</li>
+                        <li>67800 Hoenheim</li>
+                        <li>Lundi au samedi</li>
+                        <li>09h/12h et 13h/18h</li>
+                        <li>03 88 69 68 67</li>
+                    </ul>
+                </div>
+                <div id="footer__nav">
+                    <ul id="footer__menu">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="savoir-faire/savoir_faire.html">Savoir-Faire</a></li>
+                        <li><a href="erreur/erreur.html">Evénements</a></li>
+                        <li><a href="galerie/galerie.html">Galerie</a></li>
+                        <li><a href="contact/contact.html">Contact</a></li>
+                        <p class="disii">© 2019, DISII</p>
+                    </ul>
+                </div>
+            </div>
+
+        </footer>
+    </div>
 </body>
+
 </html>
--- application/views/templates_c/577ced7bad3619aa3fcaab6891f507e58435b3e4_0.file.header.tpl.php
+++ application/views/templates_c/577ced7bad3619aa3fcaab6891f507e58435b3e4_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-02-27 20:24:48
+/* Smarty version 3.1.34-dev-7, created on 2020-02-29 10:25:22
   from 'C:\wamp64\www\Ecomie\application\views\templates\header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5e5825902fc379_46880504',
+  'unifunc' => 'content_5e5a3c12598e38_31536015',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '577ced7bad3619aa3fcaab6891f507e58435b3e4' => 
     array (
       0 => 'C:\\wamp64\\www\\Ecomie\\application\\views\\templates\\header.tpl',
-      1 => 1582835086,
+      1 => 1582971913,
       2 => 'file',
     ),
   ),
@@ -20,37 +20,46 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5e5825902fc379_46880504 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5e5a3c12598e38_31536015 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<!--[if ie 6]><![endif]-->
+<html lang="fr">
 
 <head>
-	<meta charset="utf-8" />
-	<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
- - <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
-</title> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" type="image/png" href="assets/img/5151logo_fleur.ico" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Ecomie</title>
+</head>
 
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,400italic' rel='stylesheet' type='text/css'>
-
-	<style type="text/css">
-	
-		body {background-color: #fff; color: #000; width: 900px; font-family: 'Roboto Mono', 'courier', 'courier new', monotype;}
-		h1, h2 {background-color: #fff; color: #999; font-family: 'Roboto Slab', 'Times New Roman', Georgia, serif;}
-		h1 {font-size: 2em;}
-		h2 {font-size: 1.5em;}
-		em {border: solid #000 1px; padding: 0 5px; font-style: normal;}
-		label {display: inline-block; width: 10em; text-align: right;}
-		input[type="submit"] {margin: 1em 0 0 10em;}
-		.error {background-color: #ff0; color: #c00;}
-		.message {background-color: #fff; color: #0c0;}
-	
-	</style>
-</head> 
 <body>
+    <div id="principal">
+        <header>
+            <div id="logo">
+                <div class="element_logo"><img src="assets/img/logo_fleur.png" alt="Logo">
+                </div>
+                <div id="element_titre">
+                    <h1 class="titre">ECOMIE</h1>
+                    <h2 class="sous_titre">L'association d'aujourd'hui</h2>
+                </div>
+            </div>
+            <div class="menu">
+                <input class="burger" type="checkbox">
+                <nav>
+                    <a href="#">Accueil</a>
+                    <a href="savoir-faire/savoir_faire.html">Savoir-Faire</a>
+                    <a href="erreur/erreur.html">Evénements</a>
+                    <a href="galerie/galerie.html">Galerie</a>
+                    <a href="contact/contact.html">Contact</a>
+                    <a class="connexion" href="#">Connexion</a>
+                </nav>
+                <a class="connexion__mobile" href="erreur/erreur.html"><img
+                        src="assets/img/baseline_perm_identity_white_24dp.png" alt="Connexion"></a>
+
+            </div>
+        </header>
 <?php }
 }
