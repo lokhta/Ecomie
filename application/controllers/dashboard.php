50e39f5014718cc40995a2ff46fe5628900f3d4c
Sofiane Al amri <sa.dev67@gmail.com>
2020-03-07 19:01:53 +0100

    création du controller dashboard.php + les methodes d'affichage des pages

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->smarty->assign('page', 'admin/home.tpl');
+        $this->smarty->view('admin/dashboard.tpl');
+    }
+
+    public function membres(){
+        $this->smarty->assign('page', 'admin/membre.tpl');
+        $this->smarty->view('admin/dashboard.tpl');
+    }
+
+    public function articles(){
+        $this->smarty->assign('page', 'admin/article.tpl');
+        $this->smarty->view('admin/dashboard.tpl');
+    }
+
+    public function evenements(){
+        $this->smarty->assign('page', 'admin/event.tpl');
+        $this->smarty->view('admin/dashboard.tpl');
+    }
+
+    public function newsletters(){
+        $this->smarty->assign('page', 'admin/news.tpl');
+        $this->smarty->view('admin/dashboard.tpl');
+    }
+
+    public function messagerie(){
+        $this->smarty->assign('page', 'admin/messagerie.tpl');
+        $this->smarty->view('admin/dashboard.tpl');
+    }
+
+    public function archives(){
+        $this->smarty->assign('page', 'admin/archive.tpl');
+        $this->smarty->view('admin/dashboard.tpl');
+    }
+}
--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,10 +1,8 @@
 <?php
-
+defined('BASEPATH') OR exit('No direct script access allowed');
 class Pages extends CI_Controller{
     public function __construct(){
         parent::__construct();
-
-        $this->smarty->assign('base_url', base_url());
     }
 
     public function articles(){
